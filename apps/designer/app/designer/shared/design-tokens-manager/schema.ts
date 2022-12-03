3c2bf0dcfbfc95ea6eb7910d039ebed09ae4b2c8
Mateusz Burzyński <mateuszburzynski@gmail.com>
2022-12-03 17:27:39 +0100

    Cast to zod enum sources to single element tuples

--- apps/designer/app/designer/shared/design-tokens-manager/schema.ts
+++ apps/designer/app/designer/shared/design-tokens-manager/schema.ts
@@ -1,30 +1,22 @@
 import { z } from "zod";
 import { groups } from "./groups";
 
-const mapToProp = <
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  T extends ReadonlyArray<any>,
-  Prop extends keyof T[number]
->(
-  arr: readonly [...T],
-  prop: Prop
-): {
-  [K in keyof T]: T[K][Prop];
-} => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  return arr.map((item) => item[prop]) as any;
-};
-
-// zod expects readonly tuples for its enums
-const types = mapToProp(groups, "type");
-const groupNames = mapToProp(groups, "group");
-
 export const DesignToken = z.object({
   name: z.string(),
-  type: z.enum(types),
+  // zod expects readonly tuples for its enums
+  type: z.enum(
+    groups.map((group) => group.type) as unknown as readonly [
+      typeof groups[number]["type"]
+    ]
+  ),
   value: z.string(),
   description: z.optional(z.string()),
-  group: z.enum(groupNames),
+  // zod expects readonly tuples for its enums
+  group: z.enum(
+    groups.map((group) => group.type) as unknown as readonly [
+      typeof groups[number]["group"]
+    ]
+  ),
 });
 
 export type DesignToken = z.infer<typeof DesignToken>;
